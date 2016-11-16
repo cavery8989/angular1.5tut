22a860e9231261be6eb195ebeb722b6de2072b68
Chris Avery <chris.avery@accesspay.local>
2016-11-16 17:06:58 +0000

    started routing

--- ps-movies/movie-rating.component.js
+++ ps-movies/movie-rating.component.js
@@ -1,10 +1,22 @@
-(function(){
+(function (){
   'use strict'
 
-  var module = angular.module('psmovies');
-
+  var module = angular.module('psMovies');
   module.component('movieRating', {
     templateUrl: "/ps-movies/movie-rating.component.html",
+    bindings: {
+      value: '<'
+    },
+    controllerAs: 'model',
+    controller: function(){
+      var model = this;
+      model.$onInit = function () {
+        model.entries = new Array(model.value);
+      };
 
+      model.$onChanges = function () {
+        model.entries = new Array(model.value);
+      };
+    }
   });
 }());
